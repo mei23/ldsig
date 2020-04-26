e07a7a8ca9b4fabc9ecad58edf98a71efc3937d9
mei23 <m@m544.net>
2020-04-26 19:59:19 +0900

    まともなテストデータ

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ import { genKeyPair } from './utils';
 async function main() {
 	const kp = await genKeyPair();
 
-	const signed = await signRsaSignature2017({'a': 1}, kp.privateKey, 'https://example.com/users/1');
+	const data = {
+		"@context": [
+			"https://w3id.org/identity/v1",
+		],
+		"title": "a",
+		"signature": {
+			"type": "RsaSignature2017",
+			"creator": "https://mastodon.cloud/users/transmute#main-key",
+			"created": "2018-12-22T18:23:12Z",
+			"signatureValue": "dO9UeEBI5Lab4hlAkv8jpSCBPP49/LGx+7wonkhYOeC1hzRLBSMCtUPrNEseugtsu4m7cv7ZOSKiyN/d+b9eEyK/iFKkAi2oEunQOoLsX4hsm451VakuH4eFMOJh2G77+yUwuebb74zKfKFeE/KR+yh7pxkr2LuFzNYTfSTpQaMmVE1LUy5XOMIsCWIE3LL4qZAdP5cLqCdPRgqCHsSafqL0EOHunNTzE/bTrM38ptuMEL2zGQTFif3NCtNzW1SvKvZSel03KQ6uNUZbdDD8i9IYbzJrmjzYz5owY/ospVB6f3KoS0TRgYFU25EIp/a8PWHz7uNSzJkBUnT514gRvA=="
+		}
+	};
+
+	const signed = await signRsaSignature2017(data, kp.privateKey, 'https://example.com/users/1');
 	console.log(signed);
 
 	const verified = await verifyRsaSignature2017(signed, kp.publicKey);
--- test/rsa-signature-2017.ts
+++ test/rsa-signature-2017.ts
@@ -4,8 +4,21 @@ import { genKeyPair } from '../src/utils';
 
 describe('RsaSignature2017', () => {
 	it('Basic sign/verify', async () => {
+		const data = {
+			"@context": [
+				"https://w3id.org/identity/v1",
+			],
+			"title": "a",
+			"signature": {
+				"type": "RsaSignature2017",
+				"creator": "https://mastodon.cloud/users/transmute#main-key",
+				"created": "2018-12-22T18:23:12Z",
+				"signatureValue": "dO9UeEBI5Lab4hlAkv8jpSCBPP49/LGx+7wonkhYOeC1hzRLBSMCtUPrNEseugtsu4m7cv7ZOSKiyN/d+b9eEyK/iFKkAi2oEunQOoLsX4hsm451VakuH4eFMOJh2G77+yUwuebb74zKfKFeE/KR+yh7pxkr2LuFzNYTfSTpQaMmVE1LUy5XOMIsCWIE3LL4qZAdP5cLqCdPRgqCHsSafqL0EOHunNTzE/bTrM38ptuMEL2zGQTFif3NCtNzW1SvKvZSel03KQ6uNUZbdDD8i9IYbzJrmjzYz5owY/ospVB6f3KoS0TRgYFU25EIp/a8PWHz7uNSzJkBUnT514gRvA=="
+			}
+		};
+
 		const kp = await genKeyPair();
-		const signed = await signRsaSignature2017({'a': 1}, kp.privateKey, 'https://example.com/users/1');
+		const signed = await signRsaSignature2017(data, kp.privateKey, 'https://example.com/users/1');
 		const verified = await verifyRsaSignature2017(signed, kp.publicKey);
 		assert.strictEqual(verified, true);
 	});
