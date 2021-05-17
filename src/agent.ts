4da0b08a29cd05b17baef051706d7040b3112028
mei23 <m@m544.net>
2021-05-18 02:02:25 +0900

    Add DNS cache http too

--- src/agent.ts
+++ src/agent.ts
@@ -5,7 +5,8 @@ const cache = require('lookup-dns-cache');
 export const httpAgent = new http.Agent({
 		keepAlive: true,
 		keepAliveMsecs: 30 * 1000,
-	});
+		lookup: cache.lookup,
+	} as http.AgentOptions);
 
 export const httpsAgent = new https.Agent({
 		keepAlive: true,
