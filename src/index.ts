fa62e8f0da538e12a7472315abb6f9f77d5649b6
mei23 <m@m544.net>
2021-05-17 15:45:19 +0900

    Fix test

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,6 @@ async function main() {
 			"https://w3id.org/identity/v1",
 		],
 		"title": "a",
-		"signature": {
-			"type": "RsaSignature2017",
-			"creator": "https://mastodon.cloud/users/transmute#main-key",
-			"created": "2018-12-22T18:23:12Z",
-			"signatureValue": "dO9UeEBI5Lab4hlAkv8jpSCBPP49/LGx+7wonkhYOeC1hzRLBSMCtUPrNEseugtsu4m7cv7ZOSKiyN/d+b9eEyK/iFKkAi2oEunQOoLsX4hsm451VakuH4eFMOJh2G77+yUwuebb74zKfKFeE/KR+yh7pxkr2LuFzNYTfSTpQaMmVE1LUy5XOMIsCWIE3LL4qZAdP5cLqCdPRgqCHsSafqL0EOHunNTzE/bTrM38ptuMEL2zGQTFif3NCtNzW1SvKvZSel03KQ6uNUZbdDD8i9IYbzJrmjzYz5owY/ospVB6f3KoS0TRgYFU25EIp/a8PWHz7uNSzJkBUnT514gRvA=="
-		}
 	};
 
 	const signed = await ldSignature.signRsaSignature2017(data, kp.privateKey, 'https://example.com/users/1');
